843c1603adad163d2ce93ba8295d1f5b70d0256c
Elena Efimova <tipchik.89@mail.ru>
2025-05-25 02:31:00 +0100

    Спринт 4. Ефимова Елена

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# qa_python
+Спринт 4.
+Всего проведено 9 тестов.
+Тест 1. Проверка, что книга добавляется в коллекцию. test_add_new_book_add_books
+Тест 2. Проверка, что устанавливается жанр книги, если книга есть в books_genre и её жанр входит в список genre. test_set_book_genre_works
+Тест 3. Проверка, что при вводе названия книги появляется ее жанр. test_get_book_genre_find_book
+Тест 4. Проверка, что при вводе жанра выводится список книг с этим жанром. test_get_books_with_specific_genre
+Тест 5. Проверка, что возвращается корректный текущий словарь. test_get_books_genre_correct_book_genre
+Тест 6. Проверка, что выводятся книги для детей. test_get_books_for_children_genre_age_rating
+Тест 7. Проверка, что книга добавляется в избранное 1 раз. test_add_book_in_favorites_one_time
+Тест 8. Проверка, что книга удаляется из избранного. test_delete_book_from_favorites_removes
+Тест 9. Проверка, что выводится корректный список избранных книг. test_add_book_in_favorites_correct_list
+Итого: 9 тестов.
+
+platform win32 -- Python 3.13.3, pytest-8.3.5, pluggy-1.6.0 -- C:\Users\User\AppData\Local\Programs\Python\Python313\python.exe
+cachedir: .pytest_cache
+rootdir: C:\cygwin64\home\User\qa_python
+plugins: cov-6.1.1
+collected 17 items
+
+tests.py::TestBooksCollector::test_add_new_book_add_books PASSED         [  5%]
+tests.py::TestBooksCollector::test_set_book_genre_works[\u0413\u0430\u0440\u0440\u0438-\u0424\u0430\u043d\u0442\u0430\u0441\u0442\u0438\u043a\u0430] PASSED [ 11%]
+tests.py::TestBooksCollector::test_set_book_genre_works[\u041a\u0440\u0438\u043a-\u0423\u0436\u0430\u0441\u044b] PASSED [ 17%]
+tests.py::TestBooksCollector::test_set_book_genre_works[\u041c\u0430\u0448\u0430-\u041c\u0443\u043b\u044c\u0442\u0444\u0438\u043b\u044c\u043c\u044b] PASSED [ 23%]
+tests.py::TestBooksCollector::test_get_book_genre_find_book[\u0413\u0430\u0440\u0440\u0438-\u0424\u0430\u043d\u0442\u0430\u0441\u0442\u0438\u043a\u0430] PASSED [ 29%]
+tests.py::TestBooksCollector::test_get_book_genre_find_book[\u041a\u0440\u0438\u043a-\u0423\u0436\u0430\u0441\u044b] PASSED [ 35%]
+tests.py::TestBooksCollector::test_get_book_genre_find_book[\u041c\u0430\u0448\u0430-\u041c\u0443\u043b\u044c\u0442\u0444\u0438\u043b\u044c\u043c\u044b] PASSED [ 41%]
+tests.py::TestBooksCollector::test_get_books_with_specific_genre[\u0413\u0430\u0440\u0440\u0438-\u0424\u0430\u043d\u0442\u0430\u0441\u0442\u0438\u043a\u0430] PASSED [ 47%]
+tests.py::TestBooksCollector::test_get_books_with_specific_genre[\u041a\u0440\u0438\u043a-\u0423\u0436\u0430\u0441\u044b] PASSED [ 52%]
+tests.py::TestBooksCollector::test_get_books_with_specific_genre[\u041c\u0430\u0448\u0430-\u041c\u0443\u043b\u044c\u0442\u0444\u0438\u043b\u044c\u043c\u044b] PASSED [ 58%]
+tests.py::TestBooksCollector::test_get_books_genre_correct_book_genre[\u0413\u0430\u0440\u0440\u0438-\u0424\u0430\u043d\u0442\u0430\u0441\u0442\u0438\u043a\u0430] PASSED [ 64%]
+tests.py::TestBooksCollector::test_get_books_genre_correct_book_genre[\u041a\u0440\u0438\u043a-\u0423\u0436\u0430\u0441\u044b] PASSED [ 70%]
+tests.py::TestBooksCollector::test_get_books_genre_correct_book_genre[\u041c\u0430\u0448\u0430-\u041c\u0443\u043b\u044c\u0442\u0444\u0438\u043b\u044c\u043c\u044b] PASSED [ 76%]
+tests.py::TestBooksCollector::test_get_books_for_children_genre_age_rating PASSED [ 82%]
+tests.py::TestBooksCollector::test_add_book_in_favorites_one_time PASSED [ 88%]
+tests.py::TestBooksCollector::test_delete_book_from_favorites_removes PASSED [ 94%]
+tests.py::TestBooksCollector::test_add_book_in_favorites_correct_list PASSED [100%]
+
+============================= 17 passed in 0.05s ==============================
--- tests.py
+++ tests.py
@@ -1,24 +1,96 @@
+import pytest
+
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
-
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
+    # 1. проверяем, что книга добавляется в коллекцию
+    def test_add_new_book_add_books(self):
         collector = BooksCollector()
+        collector.add_new_book("Крик")
+        assert "Крик" in collector.get_books_genre()
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    # 2. проверяем, что устанавливает жанр книги, если книга есть в books_genre и её жанр входит в список genre.
+    @pytest.mark.parametrize('book,genre', [
+        ("Гарри", "Фантастика"),
+        ("Крик", "Ужасы"),
+        ("Маша", "Мультфильмы")
+    ])
+    def test_set_book_genre_works(self, book, genre):
+        collector = BooksCollector()
+        collector.add_new_book(book)
+        collector.set_book_genre(book, genre)
+        assert collector.get_book_genre(book) == genre
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    # 3. проверяем, что при вводе названия книги появляется ее жанр
+    @pytest.mark.parametrize('book,genre', [
+        ("Гарри", "Фантастика"),
+        ("Крик", "Ужасы"),
+        ("Маша", "Мультфильмы")
+    ])
+    def test_get_book_genre_find_book(self, book, genre):
+        collector = BooksCollector()
+        collector.add_new_book(book)
+        collector.set_book_genre(book, genre)
+        assert collector.get_book_genre(book) == genre
+
+    # 4. проверяем, что при вводе жанра выводится список книг с этим жанром
+    @pytest.mark.parametrize('book,genre', [
+        ("Гарри", "Фантастика"),
+        ("Крик", "Ужасы"),
+        ("Маша", "Мультфильмы")
+    ])
+    def test_get_books_with_specific_genre(self, book, genre):
+        collector = BooksCollector()
+        collector.add_new_book(book)
+        collector.set_book_genre(book, genre)
+        assert collector.get_books_with_specific_genre(genre) == [book]
+
+    # 5. проверяем, что возвращается корректный текущий словарь
+    @pytest.mark.parametrize('book,genre', [
+        ("Гарри", "Фантастика"),
+        ("Крик", "Ужасы"),
+        ("Маша", "Мультфильмы")
+    ])
+    def test_get_books_genre_correct_book_genre(self, book, genre):
+        collector = BooksCollector()
+        collector.add_new_book(book)
+        collector.set_book_genre(book, genre)
+        assert collector.get_books_genre() == {book: genre}
+
+    # 6. проверяем, что выводятся книги для детей
+    def test_get_books_for_children_genre_age_rating(self):
+        collector = BooksCollector()
+        collector.add_new_book("Крик")
+        collector.set_book_genre("Крик", "Ужасы")
+        collector.add_new_book("Маша")
+        collector.set_book_genre("Маша", "Мультфильмы")
+        assert collector.get_books_for_children() == ["Маша"]
+
+    # 7. проверяем, что книга добавляется в избранное 1 раз
+    def test_add_book_in_favorites_one_time(self):
+        collector = BooksCollector()
+        collector.add_new_book("Крик")
+        collector.add_book_in_favorites("Крик")
+        collector.add_book_in_favorites("Крик")
+        collector.add_book_in_favorites("Крик")
+        assert collector.get_list_of_favorites_books() == ["Крик"]
+
+    # 8. проверяем, что книга удаляется из избранного
+    def test_delete_book_from_favorites_removes(self):
+        collector = BooksCollector()
+        collector.add_new_book("Крик")
+        collector.add_book_in_favorites("Крик")
+        collector.delete_book_from_favorites("Крик")
+        assert "Крик" not in collector.get_list_of_favorites_books()
+
+    # 9. проверяем, что выводится корректный список избранных книг
+    def test_add_book_in_favorites_correct_list(self):
+        collector = BooksCollector()
+        collector.add_new_book("Крик")
+        collector.add_new_book("Мульт")
+        collector.add_new_book("Холмс")
+        collector.add_book_in_favorites("Крик")
+        collector.add_book_in_favorites("Мульт")
+        collector.add_book_in_favorites("Холмс")
+        assert collector.get_list_of_favorites_books() == ["Крик", "Мульт", "Холмс"]
